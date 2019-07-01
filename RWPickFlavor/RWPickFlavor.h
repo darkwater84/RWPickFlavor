51e16c0e11650a1d3ae52a3bb273768114905af4
Emanuel Pinedo <emanuel.pinedo@srax.com>
2019-07-01 12:42:42 -0700

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by EMANUEL PINEDO on 7/1/19.
+//  Copyright © 2019 EMANUEL PINEDO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
